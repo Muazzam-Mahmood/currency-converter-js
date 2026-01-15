da7c97b626739fbe1dcb3fcfb106e29649d17ccc
Muhammad Muazzam Mahmood <muazzamm024@gmail.com>
2026-01-15 23:09:44 +0500

    Initializing Project Files
    - Added all initial files (HTML, CSS, JS) to set up the project structure

---
+++ app.js
@@ -0,0 +1,57 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
+for (let select of dropdowns) {
+  for (let code in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = code;
+    newOption.value = code;
+    if (select.name === "from" && code === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && code === "PKR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let code = element.value;
+  let countryCode = countryList[code];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let image = element.parentElement.querySelector("img");
+  image.src = newSrc;
+};
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
